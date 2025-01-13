ad4f86d909b9ac0cf9b036b5dda19eb0b2bb9e1a
Iker Galardi <contacto.ikergalardi@gmail.com>
2025-01-13 16:13:37 +0100

    os: parse the /memory node in the DTB
    Currently the reading of the reg property from the /memory node is kind
    of custom to the device tree qemu gives us. The property should encode
    64 bit integer pairs (first element being the start of the memory and
    the second the size of the memory block). Instead qemu gives us garbage
    in the first 64 bit value and next is the actual pair of values.
    
    When decompiling the device tree into the device tree source, the actual
    values of the reg property are (0x0, start, 0x0, size), which does not
    make any sense.
    
    This should be looked up in the future.

--- os/kernel/dtb.c
+++ os/kernel/dtb.c
@@ -4,14 +4,21 @@
 #include "defs.h"
 #include "sbi.h"
 
-#define BYTESWAP32(num) (((num>>24)&0xff) | ((num<<8)&0xff0000) | \
-                        ((num>>8)&0xff00) | ((num<<24)&0xff000000))
+extern int strncmp(const char *p, const char *q, uint n);
+
+#define BYTESWAP32(num) ((((num)>>24)&0xff) | (((num)<<8)&0xff0000) | \
+                        (((num)>>8)&0xff00) | (((num)<<24)&0xff000000))
+
+#define BYTESWAP64(num) (BYTESWAP32(num) << 32 | BYTESWAP32((num) >> 32))
 
 #define FDT_BEGIN_NODE BYTESWAP32((uint32)0x1)
 #define FDT_END_NODE   BYTESWAP32((uint32)0x2)
 #define FDT_PROP       BYTESWAP32((uint32)0x3)
 #define FDT_NOP        BYTESWAP32((uint32)0x4)
 #define FDT_END        BYTESWAP32((uint32)0x9)
+
+uint64 ram_start = 0;
+uint64 ram_size = 0;
 
 struct fdtheader {
     uint32 magic;
@@ -30,6 +37,20 @@ struct fdtreserved {
     uint64 address;
     uint64 size;
 };
+
+static void memory_property(char *name, uint32 *property)
+{
+    if (strncmp(name, "reg", 4) == 0) {
+        uint64 *property64 = (uint64 *)property;
+
+        /// NOTE: documentation of the device tree file tells us that this
+        ///       should be a pair array (first element being the start and the
+        ///       second the size). Instead, Qemu's device tree gives us garbage
+        ///       as the first value and then the start/size pair.
+        ram_start = BYTESWAP64(*(property64 + 1));
+        ram_size = BYTESWAP64(*(property64 + 2));
+    }
+}
 
 void dtbparse(void *fdt)
 {
@@ -54,21 +75,39 @@ void dtbparse(void *fdt)
         reserved++;
     }
 
+    enum {
+        DEVICE_ROOT,
+        DEVICE_MEMORY,
+        DEVICE_UNKNOWN
+    } node = DEVICE_ROOT;
+
     uint32 *property = (uint32 *)((char *)fdt + BYTESWAP32(header->off_dt_struct));
     char *strings = (char *)fdt + BYTESWAP32(header->off_dt_strings);
     while (*property != FDT_END) {
         if (*property == FDT_BEGIN_NODE) {
             property++;
             printf("kernel(%d): node %s\n", cpuid(), (char *)property);
+
+            if (node == DEVICE_ROOT && strncmp((char *)property, "memory", 6) == 0) {
+                node = DEVICE_MEMORY;
+            }
         } else if (*property == FDT_PROP) {
             uint32 len = BYTESWAP32(*(property + 1));
             uint32 str_off = BYTESWAP32(*(property + 2));
 
             printf("kernel(%d): property %s\n", cpuid(), strings + str_off);
 
+            if (node == DEVICE_MEMORY) {
+                memory_property(strings + str_off, property+1);
+            }
+
             property += len / sizeof(uint32) + 2;
         } else if (*property == FDT_END_NODE) {
             printf("kernel(%d): end node\n", cpuid());
+
+            if (node == DEVICE_MEMORY) {
+                node = DEVICE_ROOT;
+            }
         } else if (*property == FDT_NOP) {
             property++;
             printf("kernel(%d): nop\n", cpuid());
--- os/kernel/dtb.h
+++ os/kernel/dtb.h
@@ -1,6 +1,11 @@
 #ifndef DTB_H
 #define DTB_H
 
+#include "types.h"
+
+extern uint64 ram_start;
+extern uint64 ram_size;
+
 void dtbparse(void *fdt);
 
 #endif // DTB_H

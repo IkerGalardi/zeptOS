f5415c29a98fb7a684a40f4608acf06f5c9c309f
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-07-30 12:26:49 +0200

    build: add a configuration file
    Instead of always having the security extensions enabled (as they can
    be expensive in CPU time), it would be nice to have a configuration
    file in which you can disable it.

---
+++ config.mk
@@ -0,0 +1,11 @@
+# Wether or not to enable landing pad extension support on user processes
+# Possible values:
+#   disabled: landing pads are disabled
+#   enabled:  landing pads are enabled
+CONFIG_USER_LANDING_PAD=enabled
+
+# Wether or not to enable shadow stack sanitization
+# Possible values:
+#   disabled: shadow stack is disabled
+#   hardware: shadow stack using hardware extensions
+CONFIG_USER_SHADOW_STACK=hardware

f381c306b48d9c970784f037534d19fc42a870f8
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-09-03 17:34:47 +0200

    user: benchmark mode starts the test directly
    Instead of booting the operating system normally and manually
    executing the ciphertest benchmark the operating system can be booted
    just to benchmark and shut down.
    
    This is very useful when executing benchmarks in clusters where
    interactive access is not available. Could also be used for tests.

--- user/init.c
+++ user/init.c
@@ -9,7 +9,11 @@
 #include "user/user.h"
 #include "kernel/fcntl.h"
 
+#ifndef CONFIG_BENCHMARK_ENABLED
 char *argv[] = { "sh", 0 };
+#else
+char *argv[] = { "ciphertest", 0 };
+#endif // CONFIG_BENCHMARK_ENABLED
 
 int
 main(void)
@@ -24,16 +28,26 @@ main(void)
     dup(0);    // stderr
 
     for(;;){
+#ifndef CONFIG_BENCHMARK_ENABLED
         printf("init: starting sh\n");
+#else
+        printf("init: starting ciphertest\n");
+#endif //CONFIG_BENCHMARK_ENABLED
         pid = fork();
         if(pid < 0){
             printf("init: fork failed\n");
             exit(1);
         }
         if(pid == 0){
+#ifndef CONFIG_BENCHMARK_ENABLED
             exec("sh", argv);
             printf("init: exec sh failed\n");
             exit(1);
+#else
+            exec("ciphertest", argv);
+            printf("init: exec ciphertest failed\n");
+            exit(1);
+#endif // CONFIG_BENCHMARK_ENABLED
         }
 
         for(;;){
@@ -41,8 +55,12 @@ main(void)
             // or if a parentless process exits.
             wpid = wait((int *) 0);
             if(wpid == pid){
+#ifndef CONFIG_BENCHMARK_ENABLED
                 printf("init: shell exited, restarting it\n");
                 break;
+#else
+                shutdown();
+#endif // CONFIG_BENCHMARK_ENABLED
             } else if(wpid < 0){
                 printf("init: wait returned an error\n");
                 exit(1);

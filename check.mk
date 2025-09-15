d7781d8845275cf6b3006aca3b1dd739808db3d7
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-09-15 10:45:35 +0200

    build: add support for customizing kernel landing pads

--- check.mk
+++ check.mk
@@ -4,6 +4,12 @@ $(error "Wrong value for CONFIG_USER_LANDING_PAD, should be either 'enabled' or 
 endif
 endif
 
+ifneq ($(CONFIG_KERNEL_LANDING_PAD), disabled)
+ifneq ($(CONFIG_KERNEL_LANDING_PAD), enabled)
+$(error "Wrong value for CONFIG_KERNEL_LANDING_PAD, should be either 'enabled' or 'disabled'")
+endif
+endif
+
 ifneq ($(CONFIG_USER_SHADOW_STACK), disabled)
 ifneq ($(CONFIG_USER_SHADOW_STACK), software)
 ifneq ($(CONFIG_USER_SHADOW_STACK), hardware)
--- config.mk
+++ config.mk
@@ -4,6 +4,12 @@
 #   enabled:  landing pads are enabled
 CONFIG_USER_LANDING_PAD=enabled
 
+# Wether or not to enable landing pad extension support on kernel
+# Possible values:
+#   disabled: landing pads are disabled
+#   enabled:  landing pads are enabled
+CONFIG_KERNEL_LANDING_PAD=disabled
+
 # Wether or not to enable shadow stack sanitization
 # Possible values:
 #   disabled: shadow stack is disabled

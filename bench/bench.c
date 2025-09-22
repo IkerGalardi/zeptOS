b947d6871ba759b5297b9db55f787cc98ed3715c
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-09-22 12:39:17 +0200

    bench: execute scimark on benchmark booting

--- bench/bench.c
+++ bench/bench.c
@@ -2,6 +2,7 @@
 #include "user.h"
 
 char *ciphertest_argv[] = {"ciphertest", 0};
+char *scimark4_argv[] = {"scimark4", 0};
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,13 @@ int main(int argc, char *argv[])
     int pid = fork();
     if (pid == 0) {
         exec(ciphertest_argv[0], ciphertest_argv);
+    }
+    wait(0);
+
+    printf("bench: running scimark4...\n");
+    pid = fork();
+    if (pid == 0) {
+        exec(scimark4_argv[0], scimark4_argv);
     }
     wait(0);
 }
